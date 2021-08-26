e6c419cb8b38cdc1f7f5646bfb2e7755891d3d22
Bagas <priambudibagaskara@gmail.com>
2021-08-27 01:48:02 +0700

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -99,16 +99,7 @@ module.exports = {
     },
   },
   plugins: [
-    '@docusaurus/plugin-ideal-image',
-    ['docusaurus2-dotenv',
-    {
-      path: "./.env", // The path to your environment variables.
-      safe: false, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
-      systemvars: false, // Set to true if you would rather load all system variables as well (useful for CI purposes)
-      silent: false, //  If true, all warnings will be suppressed
-      expand: false, // Allows your variables to be "expanded" for reusability within your .env file
-      defaults: false, //  Adds support for dotenv-defaults. If set to true, uses ./.env.defaults
-  }]
+    '@docusaurus/plugin-ideal-image'
   ],
   presets: [
     [
